7ae0c31eafdc01516a612e624ff5b3f0fb237a71
Anmol Gupta <anmolgupta@Anmols-MacBook-Air.local>
2025-05-01 20:14:39 +0530

    new4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello cloud new3\n'; 
+const message = 'Hello cloud new4\n'; 
 const port = 8080; 
 const server = http.createServer((req, res) => { 
   res.statusCode = 200;
