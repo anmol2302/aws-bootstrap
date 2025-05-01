449f491cc0440822e798534990f7a76b7904989f
Anmol Gupta <anmolgupta@Anmols-MacBook-Air.local>
2025-05-01 18:15:39 +0530

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,6 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Source aws_credentials.sh from the script's directory
 source "$SCRIPT_DIR/aws_credentials.sh"
 
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
 STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
@@ -21,6 +26,19 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+
+
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
---
+++ start-service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+source /home/ec2-user/.bash_profile 
+cd /home/ec2-user/app/aws-bootstrap-main/release 
+npm run start 
